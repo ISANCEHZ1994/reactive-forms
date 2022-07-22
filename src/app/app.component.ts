9554705b0ff77b0fa8b6b07029a7c2f421936b5d
Israel Sanchez <Israel.pro1994@gmail.com>
2022-07-22 15:47:10 -0500

    creating custom validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +10,23 @@ export class AppComponent implements OnInit{
 
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', "Aaron", "Anna"];
+
+  constructor(private formBuild: FormBuilder){};
 
   ngOnInit(){ 
     this.signupForm = new FormGroup({
+      'userData': new FormGroup({ // NEW GROUPING CONTROLS
+          'username': new FormControl(null, [ Validators.required, this.forbiddenNames.bind(this) ]),
+             'email': new FormControl(null, [ Validators.required, Validators.email ]),
+      }),
       // the string will represent the same 'name' in the formControlName prop in HTML file 
       // note the validation that we are using to make sure everything is correct!
-      'username': new FormControl(null, Validators.required),
-         'email': new FormControl(null, [ Validators.required, Validators.email ]),
-        'gender': new FormControl('male'),
-
+      // MOVED TO userData FormGroup({})
+      // 'username': new FormControl(null, Validators.required),
+      //    'email': new FormControl(null, [ Validators.required, Validators.email ]),
+      'gender':  new FormControl('male'),
+      'hobbies': new FormArray([])
     });
   };
 
@@ -26,5 +34,17 @@ export class AppComponent implements OnInit{
     console.log(this.signupForm);
   };
 
+  onAddHobby(){
+    const control = new FormControl( null, Validators.required );
+     (<FormArray>this.signupForm.get('hobbies')).push(control);
+  };
+
+  // Creating a Custom Validators
+  forbiddenNames( control: FormControl ): {[ s: string ]: boolean}{
+    if( this.forbiddenUsernames.indexOf(control.value) !== -1 ){
+      return { 'nameIsForbidden': true };      
+    }
+    return null;
+  };
 
 };
