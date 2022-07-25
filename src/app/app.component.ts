542f2cbe47cca05718cb2dbe538ae3aa1241e6a7
Israel Sanchez <Israel.pro1994@gmail.com>
2022-07-25 14:43:19 -0500

    setting and patching values for reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent implements OnInit{
   signupForm: FormGroup;
   forbiddenUsernames = ['Chris', "Aaron", "Anna"];
 
-  constructor(private formBuild: FormBuilder){};
+  constructor( private formBuild: FormBuilder ){};
 
   ngOnInit(){ 
     this.signupForm = new FormGroup({
@@ -33,16 +33,34 @@ export class AppComponent implements OnInit{
       'gender':  new FormControl('male'),
       'hobbies': new FormArray([])
     });
+
     // valueChanges is an observable that LISTENS to any changes that happen in the form - console.log to see the changes..
     this.signupForm.valueChanges.subscribe(
       ( value ) => console.log( value ) 
     );
+
     // statusChanges another observable that will display INVALID inside console.log UNTIL we type something inside of where the
     // specific validator is placed that matches what we are looking for - in this case the something different from (forbidden)email
     // INVALID => PENDING => VALID 
     this.signupForm.statusChanges.subscribe(
       ( status ) => console.log( status ) 
     );
+    // NOTE: Below is SIMILAR to what we have previously written in TD-Forms!!
+    // we can prepopulate the forms
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Max',
+        'email': 'max@test.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    });
+    // remember that it will update only the different variables written below and everything else will remain the same!
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Anna', // only the username will change to Anna        
+      }
+    });
   };
 
   getControls() {
